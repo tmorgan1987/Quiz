d6766f75ddf17196e09391dd0d06c6b64582f589
tmorgan1987 <tmorgan1987@outlook.com>
2022-09-25 00:39:15 -0400

    pushing

--- script.js
+++ script.js
@@ -9,39 +9,42 @@ var choiceD = document.getElementById("D");
 var quiz = document.getElementById("quiz");
 var questionDisplay = document.getElementById("questionDisplay");
 var results = document.getElementById("results");
+var userForm = document.getElementById("user-form");
+var submitBtn = document.querySelector("#submit");
+var scores = document.getElementById("scores");
 
 // Question Array
 var questions = [
 	{
-			question: "Commonly used data types DO NOT include:",
+			question: "Commonly used data types DO NOT include?",
 			choiceA: "strings",
 			choiceB: "booleans",
 			choiceC: "alerts",
 			choiceD: "numbers",
 			correct: "C"
 	},{
-			question: "The condition in an if/else statement is enclosed with __.",
+			question: "The condition in an if/else statement is enclosed with?",
 			choiceA: "quotes",
 			choiceB: "curly brackets",
 			choiceC: "parenthesis",
 			choiceD: "square brackets",
 			correct: "C"
 	},{
-			question: "Arrays in JavaScript can be used to store __.",
+			question: "Arrays in JavaScript can be used to store ???.",
 			choiceA: "numbers and strings",
 			choiceB: "other arrays",
 			choiceC: "booleans",
 			choiceD: "all of the above",
 			correct: "D"
 	},{
-			question: "String values must be enclosed within _ when being assigned to variables.",
+			question: "String values must be enclosed within ??? when being assigned to variables.",
 			choiceA: "commas",
 			choiceB: "curly brackets",
 			choiceC: "quotes",
 			choiceD: "parenthesis",
 			correct: "C"
 	},{
-			question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+			question: "A very useful tool used during development and debugging for printing content to the debugger is?",
 			choiceA: "JavaScript",
 			choiceB: "terminal/bash",
 			choiceC: "for loops",
@@ -59,10 +62,11 @@ function setTime() {
 
     if(secondsLeft <= 0) {
       clearInterval(timerInterval);
+			userInitials();
     }
 		if(noMasQuestiones === 0) {
 			clearInterval(timerInterval);
-		}
+			userInitials();}
   }, 1000);
 }
 
@@ -86,10 +90,10 @@ var currentQuestion = 0;
 // this function displays my questions and answers
 function displayQ () {
 questionDisplay.textContent = questions[currentQuestion].question;
-choiceA.innerHTML = questions[currentQuestion].choiceA;
-choiceB.innerHTML = questions[currentQuestion].choiceB;
-choiceC.innerHTML = questions[currentQuestion].choiceC;
-choiceD.innerHTML = questions[currentQuestion].choiceD;
+choiceA.textContent = questions[currentQuestion].choiceA;
+choiceB.textContent = questions[currentQuestion].choiceB;
+choiceC.textContent = questions[currentQuestion].choiceC;
+choiceD.textContent = questions[currentQuestion].choiceD;
 }
 
 var noMasQuestiones = 1;
@@ -98,9 +102,9 @@ var noMasQuestiones = 1;
 function correctAnswer (answer) {
 	console.log(answer)
 	if (answer === questions[currentQuestion].correct){
-		results.textContent = "This is the correct answer.";
+		results.textContent = "That was the correct answer.";
 	}
-	else {results.textContent = "This is the incorrect answer.";
+	else {results.textContent = "That was the incorrect answer.";
 				secondsLeft = secondsLeft-10;}
 	if (currentQuestion < lastQuestion){
 		currentQuestion++;
@@ -110,4 +114,23 @@ function correctAnswer (answer) {
 				noMasQuestiones=0;}
 }
 
-function userInitials () {}
+// this function makes the user intial form display
+function userInitials () {
+	userForm.style.display="block";
+	quiz.style.display="none";
+	timer.style.display="none";
+}
+
+submitBtn.addeventlistener("click", displayScore);
+
+
+
+function displayScore() {
+	descriptionEl.style.display="none";
+	timeEl.style.display="none";
+	quiz.style.display="none";
+	userForm.style.display="none";
+	scores.style.display="block";
+
+	displayScore();
+}
